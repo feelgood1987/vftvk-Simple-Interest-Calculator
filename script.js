fc89d09206fccdb2515830e4ca933a38737c5854
feelgood1987 <khodaei.mousa@gmail.com>
2021-07-28 21:53:01 +0430

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 function compute()
 {
     var principal = document.getElementById("principal").value,
+     if(principal==undefined || principal<1)
+     {
+         alert('The Principal value must be Positive!');
+         return;
+     }
      rate=document.getElementById("rate").value,
      years=document.getElementById("years").value;
     var interest=principal*years*rate/100;
     var year=new Date().getFullYear()+parseInt(years);
-     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "
-         +rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
-    alert(years);
+     document.getElementById("result").innerHTML="If you deposit <span class='highlight'>"+principal+"</span>,\<br\>at an interest rate of  <span class='highlight'>"
+         +rate+"%\ </span><br\>You will receive an amount of  <span class='highlight'>"+interest+" </span>,\<br\>in the year  <span class='highlight'>"+year+"\ </span><br\>";
+    
     
 }
 
