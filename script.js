926cd95eb8dbf44559a76607be08e8d6dc23c718
feelgood1987 <khodaei.mousa@gmail.com>
2021-07-28 21:28:24 +0430

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function compute()
      rate=document.getElementById("rate").value,
      years=document.getElementById("years").value;
     var interest=principal*years*rate/100;
-    var year=new Date().getFullYear+parseInt(years);
-     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    var year=new Date().getFullYear()+parseInt(years);
+     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "
+         +rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
 }
 
 function updateRate(){
